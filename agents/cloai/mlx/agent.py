af82708088fcc96a73b498e8e36a2e777c8137f9
Your Name <you@example.com>
2024-08-18 21:10:43 -0500

    Updated MLX Agent to use mlx_lm as it is now streamlined as a module.

--- agents/cloai/mlx/agent.py
+++ agents/cloai/mlx/agent.py
@@ -1,9 +1,8 @@
-from comma_agents.agents.base_agent import BaseAgent
-from comma_agents.prompts.base_prompt import PromptTemplate
-from .generate import generate
-from .models import load
-from typing import TypedDict
 import mlx.core as mx
+from mlx_lm import load, generate
+from comma_agents.agents.base_agent import BaseAgent
+from typing import TypedDict
+from comma_agents.prompts.base_prompt import PromptTemplate
 
 class MLXAgent(BaseAgent):
     class Config(TypedDict):
@@ -13,9 +12,12 @@ class MLXAgent(BaseAgent):
         seed: int
     
     def __init__(self, name, config: "MLXAgent.Config", **kwargs):
-        super().__init__(name, prompt_template=PromptTemplate("{user_message} {assistant_message}"), **kwargs)
+        super().__init__(name, **kwargs)
         self.model, self.tokenizer = load(config["model_path"])
         self.config = config
+        
+        if kwargs.get("prompt_template") is None:
+            self.prompt_template = PromptTemplate("{user_message} {assistant_message}")
         if self.config["seed"] is not None:
             mx.random.seed(self.config["seed"])
         if self.config["temp"] is None:
@@ -27,7 +29,3 @@ class MLXAgent(BaseAgent):
     def _call_llm(self, message: str) -> str:
         response = generate(self.model, self.tokenizer, message, self.config["max_tokens"], self.config["temp"])
         return response
-    
-
-    
-   
